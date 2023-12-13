4c5306f9ab37111d021145ce5091cefedb3f9164
Kailean <kailean.taguinod@gmail.com>
2023-12-13 21:11:34 +0000

    Initial Terraform stuff.

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
   zone         = var.zone
-  tags = ["dev", "web"]
+  tags         = ["dev", "web"]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
